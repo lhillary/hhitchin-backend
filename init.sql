208a90cb44b606235e0db3bc42495e3a278d784b
Lydia Hillary <44858988+lhillary@users.noreply.github.com>
2021-05-12 15:47:13 -0400

    added drop sql script and modified table creation script

--- init.sql
+++ init.sql
@@ -1,10 +1,10 @@
-CREATE TYPE accepted as ENUM ('no', 'na', 'yes');
+CREATE TYPE accepted as ENUM ('0', '1', '2');
 CREATE TABLE Contacts (
     Id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
     Name TEXT NOT NULL,
-    Phone VARCHAR(10) NOT NULL,
+    Phone VARCHAR(12) NOT NULL,
     Accepted accepted
 );
 
 INSERT INTO Contacts (Name, Phone, Accepted) 
-VALUES ('Lydia Hillary', '3137275279', 'na');
+VALUES ('Lydia Hillary', '+13137275279', '0');
